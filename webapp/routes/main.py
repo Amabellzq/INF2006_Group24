67983d120d8148d15800ec71fc5c479b806e2ccf
Jun Wei <2202187@Sit.singaporetech.edu.sg>
2025-02-01 01:27:16 +0800

    Update Homepage

--- webapp/routes/main.py
+++ webapp/routes/main.py
@@ -10,7 +10,26 @@ def health_check():
         'database': 'connected' if db.session.connection() else 'disconnected'
     })
 
+
+from flask import Blueprint, render_template
+from webapp.models import Product
+from webapp.extensions import db
+
+main_bp = Blueprint('main', __name__)
+
+
 @main_bp.route('/')
 def home():
-    # Render the home.html template
-    return render_template('home.html')
+    """Fetch all products and separate Flash Deals from regular products."""
+
+    # Flash Deals: Products that have a discount price
+    flash_deals = Product.query.filter(
+        Product.discount_price.isnot(None)
+    ).order_by(Product.created_at.desc()).all()
+
+    # Regular Products: Products that do NOT have a discount price
+    products = Product.query.filter(
+        Product.discount_price.is_(None)
+    ).order_by(Product.created_at.desc()).all()
+
+    return render_template('home.html', flash_deals=flash_deals, products=products)
