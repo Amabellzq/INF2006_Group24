dc932c022c4760b61d99669161437df8af5421fb
Jun Wei <2202187@Sit.singaporetech.edu.sg>
2025-02-16 13:51:01 +0800

    S3 Update

--- webapp/extensions.py
+++ webapp/extensions.py
@@ -1,12 +1,10 @@
 # webapp/extensions.py
+import boto3
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
-
-# Initialize S3 Client using IAM Role authentication
-s3 = boto3.client("s3")
 
 db = SQLAlchemy()
 migrate = Migrate()
